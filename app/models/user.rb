a87b36c9c2c5d53549933c7b946a59e0c1102ec1
shubham-khatri-fb <shubham.khatri@flobiz.in>
2022-07-07 20:12:39 +0530

    Modified validation of password presence on creation only

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
     has_many :user_transactions
     has_many :user_currencies
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    validates_presence_of  :password, on: :create
+    validates :password, presence: {:message => "Password can't be left blank."}, on: :create
 
     validates :full_name, presence: {message: "Name can't be left blank!!"} 
     validates :phone_number, presence: true, length: {is: 10}, uniqueness: {:message => "Phone Number already exists."}
-    validates :password, presence: {:message => "Password can't be left blank."}
+    
 
     mount_uploader:avatar, AvatarUploader
 end
